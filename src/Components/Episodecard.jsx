10a21ce74a097b6f160bb157258987323642eee4
Keshav Gupta <106925920+keshav-gupta19@users.noreply.github.com>
2024-02-10 23:01:54 +0530

    Update Episodecard.jsx

--- src/Components/Episodecard.jsx
+++ src/Components/Episodecard.jsx
@@ -64,7 +64,7 @@ const Episodecard = ({ episode, podid, user, type, index }) => {
     try {
       // console.log(podid?._id);
       await axios.post(
-        `http://localhost:3000/api/podcast/addorview/${podid?._id}`
+        `https://castwave-api-podcast.onrender.com/api/podcast/addorview/${podid?._id}`
       );
       // console.log(res);
     } catch (error) {
